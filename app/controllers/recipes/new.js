b844c3531db5ef19e4c09384a13b8802f3acd6af
Melanie Gilman <melanie@thoughtbot.com>
2015-01-18 16:09:28 -0500

    Getting closer. There are still empty li elems

---
+++ app/controllers/recipes/new.js
@@ -0,0 +1,17 @@
+import Ember from "ember";
+
+export default Ember.ObjectController.extend({
+  newRecipe: function() {
+    return this.store.createRecord("recipe");
+  }.property(),
+  actions: {
+    createRecipe: function() {
+      var controller = this;
+      var recipes = this.get("model");
+      this.get("newRecipe").save().then(function(recipe){
+        recipes.addObject("recipe");
+        controller.set("newRecipe", controller.store.createRecord("recipe"))
+      })
+    }
+  }
+});
