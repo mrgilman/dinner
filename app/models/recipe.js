7b3b4a7ce56998f379c6c6b83976a6a8446f047d
Melanie Gilman <melanie@thoughtbot.com>
2015-01-18 20:48:52 -0500

    Remove image field from recipe

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -2,8 +2,7 @@ import DS from "ember-data";
 
 var Recipe = DS.Model.extend({
   title: DS.attr("string"),
-  url: DS.attr("string"),
-  image: DS.attr("string")
+  url: DS.attr("string")
 });
 
 Recipe.reopenClass({
@@ -11,14 +10,12 @@ Recipe.reopenClass({
     {
       id: 1,
       title: "pull apart cheesy garlic knots",
-      url: "http://comfortablefood.com/pull-apart-cheesy-garlic-knots/",
-      image: "http://comfortablefood.com/wp-content/uploads/2013/05/cheesy_garlic_pull_apart_knots1.jpg"
+      url: "http://comfortablefood.com/pull-apart-cheesy-garlic-knots/"
     },
     {
       id: 2,
       title: "Creamy Chicken Stew",
-      url: "http://norecipes.com/creamy-chicken-stew/",
-      image: "http://cdn2.norecipes.com/wp-content/uploads/2015/01/creamy-chicken-stew-4.jpg?883503"
+      url: "http://norecipes.com/creamy-chicken-stew/"
     }
   ]
 });
