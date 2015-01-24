25b80ea1a764b8fc3479004454806fe70f07c78c
Melanie Gilman <melanie@thoughtbot.com>
2015-01-23 21:16:25 -0500

    Switch to using ActiveModel adapter

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,23 +1,6 @@
 import DS from "ember-data";
 
-var Recipe = DS.Model.extend({
+export default DS.Model.extend({
   title: DS.attr("string"),
   url: DS.attr("string")
 });
-
-Recipe.reopenClass({
-  FIXTURES: [
-    {
-      id: 1,
-      title: "pull apart cheesy garlic knots",
-      url: "http://comfortablefood.com/pull-apart-cheesy-garlic-knots/"
-    },
-    {
-      id: 2,
-      title: "Creamy Chicken Stew",
-      url: "http://norecipes.com/creamy-chicken-stew/"
-    }
-  ]
-});
-
-export default Recipe;
